cc5a801f1cf01d04ac5203f8070d5aaac8717676
Ariful491 <arifulhoque091@gmail.com>
2024-06-04 01:45:17 +0600

    signUp Completed.

--- src/app/_components/RestaurantLogin.js
+++ src/app/_components/RestaurantLogin.js
@@ -1,6 +1,25 @@
 import {Card, Col, Container, Row} from "@/app/_components/helperComponents/BootstrapStyleHelper";
+import {useState} from "react";
 
 const RestaurantLogin = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(false);
+
+
+    const handleLogin = async () => {
+      if (!email || !password){
+          setError(true)
+          return null;
+      }else{
+          setError(false);
+      }
+      console.log(email,password)
+
+     // const response = await fetch('http://localhost:300/api/');
+
+    }
+
     return (
         <>
             <Container>
@@ -12,13 +31,33 @@ const RestaurantLogin = () => {
                                     <h4>Login</h4>
                                 </div>
                                 <div className='m-1 p-2'>
-                                    <input type="email" className="form-control " placeholder="Enter email id"/>
+                                    <input type="email" className="form-control "
+                                           value={email}
+                                           onChange={(e)=>setEmail(e.target.value)}
+                                           placeholder="Enter email id"/>
+                                    <div className={'text-start'}>
+                                        {
+                                            error && !email ?
+                                                <span className={'text-danger'}>Email is required.</span>
+                                                : ''
+                                        }
+                                    </div>
                                 </div>
                                 <div className='m-1 p-2'>
-                                    <input type="password" className=" form-control  " placeholder="Enter password"/>
+                                    <input type="password" className=" form-control  "
+                                           value={password}
+                                           onChange={(e)=>setPassword(e.target.value)}
+                                           placeholder="Enter password"/>
+                                    <div className={'text-start'}>
+                                        {
+                                            error && !password ?
+                                                <span className={'text-danger'}>Password is required.</span>
+                                                : ''
+                                        }
+                                    </div>
                                 </div>
                                 <div className='m-1 p-3'>
-                                    <button className="  btn btn-primary"> Login</button>
+                                    <button onClick={handleLogin} className="  btn btn-primary"> Login</button>
                                 </div>
                             </Card>
                         </div>
--- src/app/_components/RestaurantSignUp.js
+++ src/app/_components/RestaurantSignUp.js
@@ -14,9 +14,28 @@ const RestaurantSignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState(false);
+    const [errorConfirmPassword, setErrorConfirmPassword] = useState(false);
 
 
     const handleSingUp = async () => {
+        if (password !== confirmPassword) {
+            setErrorConfirmPassword(true);
+            return null;
+        } else {
+            setErrorConfirmPassword(false);
+
+        }
+        if (!name || !restaurantName || !city || !address || !contact || !email || !password || !confirmPassword) {
+            setError(true)
+            return null;
+        } else {
+            setError(false);
+        }
+
+
+
+
         let response = await fetch("http://localhost:3000/api/restaurant", {
             method: "POST",
             body: JSON.stringify({name, restaurantName, city, address, contact, email, password})
@@ -49,6 +68,13 @@ const RestaurantSignUp = () => {
                                            value={name}
                                            onChange={(e) => setName(e.target.value)}
                                            placeholder="Name"/>
+                                    <div className={'text-start'}>
+                                        {
+                                            error && !name ?
+                                                <span className={'text-danger'}>Name is required.</span>
+                                                : ''
+                                        }
+                                    </div>
                                 </div>
 
                                 <div className="p-1 m-1">
@@ -56,6 +82,13 @@ const RestaurantSignUp = () => {
                                            value={restaurantName}
                                            onChange={(e) => setRestaurantName(e.target.value)}
                                            placeholder="Enter restaurant name"/>
+                                    <div className={'text-start'}>
+                                        {
+                                            error && !name ?
+                                                <span className={'text-danger'}>Restaurant name is required.</span>
+                                                : ''
+                                        }
+                                    </div>
                                 </div>
 
                                 <div className="p-1 m-1">
@@ -63,6 +96,13 @@ const RestaurantSignUp = () => {
                                            value={city}
                                            onChange={(e) => setCity(e.target.value)}
                                            className="form-control" placeholder="Enter city name"/>
+                                    <div className={'text-start'}>
+                                        {
+                                            error && !city ?
+                                                <span className={'text-danger'}>City name is required.</span>
+                                                : ''
+                                        }
+                                    </div>
                                 </div>
 
                                 <div className="p-1 m-1">
@@ -70,12 +110,30 @@ const RestaurantSignUp = () => {
                                            value={address}
                                            onChange={(e) => setAddress(e.target.value)}
                                            className="form-control" placeholder="Enter full address "/>
+
+                                    <div className={'text-start'}>
+                                        {
+                                            error && !address ?
+                                                <span className={'text-danger'}>Address  is required.</span>
+                                                : ''
+                                        }
+                                    </div>
+
                                 </div>
                                 <div className="p-1 m-1">
                                     <input type="text"
                                            value={contact}
                                            onChange={(e) => setContact(e.target.value)}
                                            className="form-control" placeholder="Enter contact number "/>
+
+
+                                    <div className={'text-start'}>
+                                        {
+                                            error && !contact ?
+                                                <span className={'text-danger'}>Contact number is required.</span>
+                                                : ''
+                                        }
+                                    </div>
                                 </div>
 
                                 <div className="p-1 m-1">
@@ -83,12 +141,28 @@ const RestaurantSignUp = () => {
                                            value={email}
                                            onChange={(e) => setEmail(e.target.value)}
                                            className="form-control" placeholder="Enter email id"/>
+                                    <div className={'text-start'}>
+                                        {
+                                            error && !email ?
+                                                <span className={'text-danger'}>Email is required.</span>
+                                                : ''
+                                        }
+                                    </div>
                                 </div>
                                 <div className="p-1 m-1">
                                     <input type="password"
                                            value={password}
                                            onChange={(e) => setPassword(e.target.value)}
                                            className="form-control" placeholder="Enter password"/>
+
+                                    <div className={'text-start'}>
+                                        {
+                                            error && !password ?
+                                                <span className={'text-danger'}>Restaurant name is required.</span>
+                                                : ''
+                                        }
+                                    </div>
+
                                 </div>
                                 <div className="p-1 m-1">
                                     <input type="password"
@@ -96,6 +170,14 @@ const RestaurantSignUp = () => {
                                            onChange={(e) => setConfirmPassword(e.target.value)}
                                            className="form-control"
                                            placeholder="Enter confirm password"/>
+
+                                    <div className={'text-start'}>
+                                        {
+                                            errorConfirmPassword ?
+                                                <span className={'text-danger'}> Confirm password doesn't match.</span>
+                                                : ''
+                                        }
+                                    </div>
                                 </div>
                                 <div className="p-1 m-1">
                                     <button className="btn btn-primary" onClick={handleSingUp}> sign up</button>
