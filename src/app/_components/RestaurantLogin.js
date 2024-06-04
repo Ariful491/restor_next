131acda5354da010946af0b5f85cf55b8cbaaca7
Ariful491 <arifulhoque091@gmail.com>
2024-06-05 00:56:46 +0600

    login  Completed.

--- src/app/_components/RestaurantLogin.js
+++ src/app/_components/RestaurantLogin.js
@@ -1,22 +1,37 @@
 import {Card, Col, Container, Row} from "@/app/_components/helperComponents/BootstrapStyleHelper";
 import {useState} from "react";
+import {useRouter} from "next/navigation";
 
 const RestaurantLogin = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(false);
+    const type = true;
 
+    const router = useRouter();
 
     const handleLogin = async () => {
-      if (!email || !password){
-          setError(true)
-          return null;
-      }else{
-          setError(false);
-      }
-      console.log(email,password)
+        if (!email || !password) {
+            setError(true)
+            return null;
+        } else {
+            setError(false);
+        }
 
-     // const response = await fetch('http://localhost:300/api/');
+        let response = await fetch("http://localhost:3000/api/restaurant", {
+            method: "POST",
+            body: JSON.stringify({email, password, type})
+        })
+        response = await response.json();
+        if (response.success) {
+            alert("Restaurant success fully signup .")
+            const {result} = response;
+            delete result.password;
+            localStorage.setItem('user', JSON.stringify(result));
+            router.push('/restaurant/dashboard');
+        } else {
+            alert(response.error);
+        }
 
     }
 
@@ -24,16 +39,17 @@ const RestaurantLogin = () => {
         <>
             <Container>
                 <Row>
-                    <Col md='6' className='ms-auto' >
+                    <Col md='6' className='ms-auto'>
                         <div>
                             <Card className='shadow-lg mt-4  bg-white bg-opacity-75 rounded-3'>
-                                <div className='card-header bg-success bg-opacity-75 text-white shadow   p-2 mb-4 rounded-3'>
+                                <div
+                                    className='card-header bg-success bg-opacity-75 text-white shadow   p-2 mb-4 rounded-3'>
                                     <h4>Login</h4>
                                 </div>
                                 <div className='m-1 p-2'>
                                     <input type="email" className="form-control "
                                            value={email}
-                                           onChange={(e)=>setEmail(e.target.value)}
+                                           onChange={(e) => setEmail(e.target.value)}
                                            placeholder="Enter email id"/>
                                     <div className={'text-start'}>
                                         {
@@ -46,7 +62,7 @@ const RestaurantLogin = () => {
                                 <div className='m-1 p-2'>
                                     <input type="password" className=" form-control  "
                                            value={password}
-                                           onChange={(e)=>setPassword(e.target.value)}
+                                           onChange={(e) => setPassword(e.target.value)}
                                            placeholder="Enter password"/>
                                     <div className={'text-start'}>
                                         {
--- src/app/api/restaurant/route.js
+++ src/app/api/restaurant/route.js
@@ -7,7 +7,6 @@ export async function GET() {
     await dbConnect();
     try {
         const users = await userSchema.find({});
-        console.log(users);
         return NextResponse.json({result: users})
     } catch (error) {
 
@@ -20,11 +19,20 @@ export async function POST(request) {
     try {
         await dbConnect();
         const payload = await request.json();
+        if (payload.type) {
+            const users = await userSchema.findOne({email: payload.email, password: payload.password}).exec();
+            if (!users) {
+                throw ("email & password doesn't match.");
+            }
+            return NextResponse.json({result: users, success: true});
+        }
+
         const user = new userSchema(payload);
         const savedUser = await user.save();
         return NextResponse.json({result: savedUser, success: true});
     } catch (error) {
-        return NextResponse.json({error: error.message, success: false});
+        let errorMessage = !error.message ? error : error.message;
+        return NextResponse.json({error: errorMessage, success: false});
     }
 
 }
